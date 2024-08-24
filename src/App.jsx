2e30c2f7ab20c376a8c06f31600244d5be3f22c3
David Werfelmann <werfelmann@Davids-MBP.attlocal.net>
2024-08-24 14:06:54 -0500

    Banner and App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import useState from 'react';
+import satData from './components/satData';
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))]; 
+  /*  
+    spread operator converts the new Set object into an array
+    new Set() creates a new Set object, which removes duplicates from its collection
+    satData.map() will iterate over the satData array
+    for each element of the satData, we return the values associated with orbitType keys
+  */
+
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat}/>
     </div>
   );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Banner() {
+    return (
+        <div className="banner">
+            <header>Orbit Report</header>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
